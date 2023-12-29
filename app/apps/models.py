ad9e5dadfb6bbb0cd2769f32d70cdd019ec74683
Озод <86515876+SaD-Pr0gEr@users.noreply.github.com>
2023-12-29 22:57:37 +0500

    Base model mixin

--- app/apps/models.py
+++ app/apps/models.py
@@ -1,6 +1,15 @@
-from typing import Any
+from typing import Any, Union
 
-from sqlalchemy import JSON, MetaData
+from sqlalchemy import (
+    JSON,
+    Delete,
+    MetaData,
+    ScalarResult,
+    Select,
+    Update,
+    select,
+)
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -16,3 +25,61 @@ class BASE(DeclarativeBase):
         }
     )
     type_annotation_map = {dict[int | str, Any]: JSON}
+
+
+class BaseModelMixin:
+    @classmethod
+    def _filter_field(
+        cls,
+        query: Union[Select[Any], Delete, Update],
+        field_name: str,
+        field_value: str | int | bool,
+    ) -> Union[Select[Any], Delete, Update]:
+        field_name, expr = field_name.split("__")
+        if expr == "eq":
+            query = query.where(getattr(cls, field_name) == field_value)
+        elif expr == "ne":
+            query = query.where(getattr(cls, field_name) != field_value)
+        elif expr == "gt":
+            query = query.where(getattr(cls, field_name) > field_value)
+        elif expr == "ge":
+            query = query.where(getattr(cls, field_name) >= field_value)
+        elif expr == "lt":
+            query = query.where(getattr(cls, field_name) < field_value)
+        elif expr == "le":
+            query = query.where(getattr(cls, field_name) <= field_value)
+        else:
+            query = query.where(getattr(cls, field_name) == field_value)
+        return query
+
+    @classmethod
+    def filter(
+        cls,
+        query: Union[Select[Any], Delete, Update],
+        filter_fields: dict,
+    ) -> Union[Select[Any], Delete, Update]:
+        for field, value in filter_fields.items():
+            query = cls._filter_field(query, field, value)
+        return query
+
+    @classmethod
+    async def select_query(
+        cls, session: AsyncSession, filter_fields: dict | None = None
+    ) -> ScalarResult[Any]:
+        query: Select = select(cls)
+        if filter_fields:
+            query = cls.filter(query, filter_fields)  # type: ignore
+        result = await session.execute(query)
+        return result.scalars()
+
+    @classmethod
+    async def get_all(
+        cls, session: AsyncSession, filter_fields: dict | None = None
+    ) -> Any:
+        return (await cls.select_query(session, filter_fields)).all()
+
+    @classmethod
+    async def get_first(
+        cls, session: AsyncSession, filter_fields: dict | None = None
+    ) -> Any:
+        return (await cls.select_query(session, filter_fields)).first()
