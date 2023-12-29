6fbd03744c8a5fe6a47eb626eb36b6de0c39bb3e
Озод <86515876+SaD-Pr0gEr@users.noreply.github.com>
2023-12-29 22:58:35 +0500

    Model mixin inheritance

--- app/apps/api/v1/core/models.py
+++ app/apps/api/v1/core/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.apps.models import BASE
+from app.apps.models import BASE, BaseModelMixin
 
 
-class Core(BASE):
+class Core(BASE, BaseModelMixin):
     __tablename__ = "core"
 
     id: Mapped[int] = mapped_column(primary_key=True)
